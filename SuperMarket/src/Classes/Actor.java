3c96599204f3aeda0ba8df88eb615787c8f4a830
Artemiev Dima <adbart1985@gmail.com>
2023-04-25 19:57:22 +0300

    Solved HW2

--- SuperMarket/src/Classes/Actor.java
+++ SuperMarket/src/Classes/Actor.java
@@ -1,16 +1,33 @@
 package Classes;
 
 import Interfaces.iActorBehaviour;
-
-public abstract class Actor implements iActorBehaviour {
+import Interfaces.iReturnOrder;
+/** базовый (родительский) класс посетителей */
+public abstract class Actor implements iActorBehaviour, iReturnOrder {
+    /** имя */
     protected String name;
+    /** статус выполнения заказа */
     protected boolean isTakeOrder;
+    /** статус получения заказа  */
     protected boolean isMakeOrder;
+    /** статус оплаты  */
+    protected boolean isTakeCash;
+    /** статус возврата заказа */
+    protected boolean isMakeReturnOrder;
     
+    /**
+     * конструктор класса
+     * @param name имя
+     */
     public Actor(String name) {
         this.name = name;
     }
-
-    public abstract String getName(); 
+    /**
+     * гет метод для получения имени
+     * @return имя
+     */
+    public  String getName(){
+        return name;
+    } 
 
 }
--- SuperMarket/src/Classes/OrdinaryClient.java
+++ SuperMarket/src/Classes/OrdinaryClient.java
@@ -1,41 +1,75 @@
 package Classes;
 
+/** класс Обычный клиент */
 public class OrdinaryClient extends Actor {
 
+    /**
+     * Базовый конструктор класса
+     * @param name Имя
+     */
     public OrdinaryClient(String name)
     {
         super(name);
     }
 
+    /** Переопределение метода для получение Имени */
     @Override
     public String getName() {        
         return super.name;
     }
 
+    /** переопределение метода: сделал ли заказ */
     @Override
     public boolean isMakeOrder() {
         return super.isMakeOrder;
     }
 
+    /** переопределение метода: забрал ли заказ */
     @Override
     public boolean isTakeOrder() {
         return super.isTakeOrder;
     }
-
+    
+    /** Сеттер. переопределение метода : сделал заказ  */
     @Override
     public void setMakeOrder(boolean makeOrder) {
        super.isMakeOrder = makeOrder;
     }
 
+    /** Сеттер.перопределение метода: получил заказ */
     @Override
     public void setTakeOrder(boolean pickUpOrder) {
        super.isTakeOrder = pickUpOrder;   
     }
 
+    /** переопределение метода: получить Клиент */
     @Override
     public Actor getActor() {
         return this;
     }
 
+    /** переопределение метода: есть ли заявка на возврат товара */
+    @Override
+    public boolean isMakeReturnOrder() {
+        return super.isMakeReturnOrder;
+    }
+
+    /* переопределение метода забрал ли деньги */
+    @Override
+    public boolean isTakeCash() {
+        return super.isTakeCash;
+    }
+
+    /* переопределение метода сделал заявку на возврат */
+    @Override
+    public void setMakeReturnOrder(boolean makeReturnOrder) {
+        super.isMakeReturnOrder = makeReturnOrder;
+    }
+
+    /* переопределение метода забрать деньги */
+    @Override
+    public void setTakeCash(boolean takeCash) {
+        super.isTakeCash = takeCash;
+    }
     
 }
